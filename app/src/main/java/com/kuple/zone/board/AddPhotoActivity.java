5b6093a8f7bc8f3f81d6b04e7379ce9f1747e83b
beongyoung <50867167+beongyoung@users.noreply.github.com>
2020-08-05 10:53:16 +0900

    feed 수정

--- app/src/main/java/com/kuple/zone/board/AddPhotoActivity.java
+++
@@ -1,4 +0,0 @@
-package com.kuple.zone.board;
-
-public class AddPhotoActivity {
-}
--- app/src/main/java/com/kuple/zone/navigation/HomeFragment.java
+++ app/src/main/java/com/kuple/zone/navigation/HomeFragment.java
@@ -7,12 +7,22 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.kuple.zone.R;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
+    private RecyclerView feedview;
+    private FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_alarm, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        feedview = view.findViewById(R.id.feed_kuple);
         return view;
     }
 
